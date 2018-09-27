4c36952845f130997e5021c445a3e83b2c175bc1
jh <873750974@qq.com>
2018-09-27 16:25:57 +0800

    个位数运算后缀表达式

---
+++ JAVA/PostixExpression/src/PostixExpression.java
@@ -0,0 +1,66 @@
+/**
+ * 直接根据数据结构与算法分析上讲的写的一个生成后缀表达式的算法，不过目前只能生成个位数运算的表达式，还有待改进。
+ * */
+import java.util.*;
+public class PostixExpression {
+	public static char [] c = {'+','-','*','/','(',')'};
+	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
+		String s = input.next();
+		char[] x = toSuffix(s);
+		for(int i=0;i<x.length;i++) {
+			System.out.print(x[i]);
+		}
+	}
+	
+	public static char[] toSuffix(String expression) {
+		Stack suffix = new Stack();
+		Stack op = new Stack();
+		for(int i =0;i<expression.length();i++) {
+			//System.out.println("i="+i);
+			char ch = expression.charAt(i);
+			if(ch<'9'&&ch>'0') {
+				suffix.add(ch);
+			}else if(op.size()==0){
+				op.add(ch);
+			}else {
+				int pop = getIndex((char)op.peek());
+				int add = getIndex(ch);
+				//System.out.println("pop="+pop);
+				//System.out.println("add="+add);
+				if(add<pop&&pop!=4) {
+					while( op.size()!=0 && getIndex((char)op.peek())!=4 ) {
+						suffix.add(op.pop());
+					}
+					op.add(ch);
+				}else if(add==5){
+					while( op.size()!=0 && getIndex((char)op.peek())!=4) {
+						suffix.add(op.pop());
+					}
+					op.pop();
+				}else {
+					op.add(ch);
+				}
+			}
+		}
+		while(op.size()!=0) {
+			suffix.add(op.pop());
+		}
+		int y =suffix.size();
+		char fin[] =  new char[y];
+		for(int i=y-1;i>=0;i--) {
+			fin[i]=(char)suffix.pop();
+		}
+		
+		return fin;
+	}
+
+	public static int getIndex(char x) {
+		for(int i=0;i<c.length;i++) {
+			if(x==c[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
