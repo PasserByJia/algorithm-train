b1c2d80d26e3ede1be0436bee74b7d679502fef5
jh <873750974@qq.com>
2018-10-05 18:55:12 +0800

    问题修正

--- JAVA/HDUOJ1257/src/Main.java
+++ JAVA/HDUOJ1257/src/Main.java
@@ -3,6 +3,37 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input  = new Scanner(System.in);
+		while(input.hasNext()) {
+			int n = input.nextInt();
+			int[] dp =  new int[n+1];
+			int[] a =  new int[n+1];
+			for(int i =1;i<=n;i++) {
+				a[i] = input.nextInt();
+				dp[i]=1;
+			}
+			
+			for(int i=1;i<=n;i++) {
+				for(int j =1;j<=i;j++) {
+					if(a[i]>a[j]) {
+						dp[i]=Math.max(dp[i], dp[j]+1);
+					}
+				}
+			}
+			
+			int ans = 0;
+	        for (int i = 1; i <= n; i++)
+	            ans = Math.max(ans, dp[i]);
+	        System.out.printf("%d\n", ans);
+		}
+		
+	}
+}
+/*
+ *此解法应该并没有错误，但是java代码提交到HDUOJ里就是错误的情况
+ import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input  = new Scanner(System.in);
 		int n = input.nextInt();
 		ArrayList<Integer> arr = new ArrayList();
 		for(int i=0;i<n;i++) {
@@ -28,3 +59,4 @@ public class Main {
 		System.out.println(count);
 	}
 }
+ **/
