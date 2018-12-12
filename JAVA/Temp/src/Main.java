ba32bcead84a1c9a1f78da7104d34404bba5c34e
jh <873750974@qq.com>
2018-12-12 21:40:43 +0800

    冒泡

--- JAVA/Temp/src/Main.java
+++ JAVA/Temp/src/Main.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int m = input.nextInt();
-        if(m==0)return;
-        for(int i = 0;i < m;i++){
-            int n = input.nextInt();
-            if(n==0) {
-            	//input.nextLine();
-            	//input.nextLine();
-            	continue;
-            }
-            int[] number = new int[n];
-            for(int j = 0;j < n;j++){
-                number[j] =  input.nextInt();
-            }
-            for(int k=0;k<n-1;k++){
-                for(int l=k+1;l<n;l++){
-                    if(Math.abs(number[k])<Math.abs(number[l])){
-                        int temp = number[k];
-                        number[k]= number[l];
-                        number[l]=temp;
-                    } 
-                } 
-            }
-            for(int z= 0;z<n;z++){
-                if(z!=n-1){
-                    System.out.print(number[z]+" ");
-                }else{
-                    System.out.println(number[z]);
-                }
-            }
-        }
+        solution2(10,3);
     }
+	public static int solution2(int nLevel, int kChess) {
+		if (kChess == 1) {
+			return nLevel;
+		}
+		int[][] dp = new int[nLevel + 1][kChess + 1];
+		for (int i = 1; i != dp.length; i++) {
+			dp[i][1] = i;
+		}
+		for (int i = 1; i != dp.length; i++) {
+			for (int j = 2; j != dp[0].length; j++) {
+				int min = Integer.MAX_VALUE;
+				for (int k = 1; k != i + 1; k++) {
+					min = Math.min(min,
+							Math.max(dp[k - 1][j - 1], dp[i - k][j]));
+				}
+				dp[i][j] = min + 1;
+				System.out.print(dp[i][j]+" ");
+			}
+			System.out.println();
+		}
+		return dp[nLevel][kChess];
+	}
 }
