a43cafa2eb79f14db2b2c287004cdf2535e43b1f
jh <873750974@qq.com>
2019-01-03 14:51:22 +0800

    PATB1009 字符串模拟

--- JAVA/PATB1009/src/Main.java
+++ JAVA/PATB1009/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String str = input.nextLine();
-		String [] arr = str.split("\\s+");
+		String [] arr = str.split("\\s+");//Split string with space
 		for(int i = arr.length-1;i>=0;i-- ) {
 			System.out.print(arr[i]);
 			if(i>0)System.out.print(" ");
