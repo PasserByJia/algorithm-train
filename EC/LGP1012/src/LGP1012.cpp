3fdd585543563c0e04c98412242310c37d69053b
jh <873750974@qq.com>
2019-02-01 16:40:25 +0800

    LG字符串训练

---
+++ EC/LGP1012/src/LGP1012.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : LGP1012.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std;
+string a[21];
+bool cmp(string a,string b)
+{
+    return a+b>b+a;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    cin>>a[i];
+    sort(a+1,a+n+1,cmp);
+    for(int i=1;i<=n;i++)cout<<a[i];
+}
