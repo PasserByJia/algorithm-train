b280806a08205eb5f024d82b763412f13c276c61
jh <873750974@qq.com>
2019-02-14 15:11:10 +0800

    蓝桥杯

--- JAVA/LQC3P5/src/Main.java
+++ JAVA/LQC3P5/src/Main.java
@@ -1,83 +1,189 @@
 import java.util.Scanner;
  
 public class Main {
+public static int[][] vis ;
+public static char[][] array ;
+public static int count;
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		while (n-- != 0) {
-			char[][] array = new char[3][4];
 			String temp;
-			// put in
+			count =999;
+			vis =new int[3][4];
+			array = new char[3][4];
 			for (int i = 0; i < 3; i++) {
-				temp = s.next();
-				for (int j = 0; j < 4; j++)
-					array[i][j] = temp.charAt(j);
- 
-			}
-			// find 0
-			int time = 0;
-			for (int i = 0; i < 3; i++) {
+				temp = s.next();//读入一行
 				for (int j = 0; j < 4; j++) {
-					if (array[i][j] == '0' && addrow(array, i, j)) {
-						time=1;
-						break;
-					}
-					if (array[i][j] == '0' && addline(array, i, j)) {
-						time=1;
-						break;
+					array[i][j] = temp.charAt(j);//拆出单个
+					if(array[i][j]!='0') {
+						vis[i][j]=-1;
 					}
 				}
-				if(time==1){
-					break;
-				}
- 
 			}
- 
+			int x=-1,y=-1;
+			char ss ='/';
+			init();
+			for(int i=0;i<3;i++) {
+				int temp2 = count;
+				for (int j = 0; j < 4; j++) {
+					if(vis[i][j]==0) {
+						array[i][j]='1';
+						vis[i][j]=-1;
+						search(0);
+						if(count<temp2) {
+							x=i;y=j;ss=array[i][j];
+						}
+						array[i][j]='0';
+						vis[i][j]=0;
+						array[i][j]='-';
+						vis[i][j]=-1;
+						search(0);
+						if(count<temp2) {
+							x=i;y=j;ss=array[i][j];
+						}
+						array[i][j]='0';
+						vis[i][j]=0;
+					}else if(vis[i][j]==1){
+						array[i][j]='-';
+						vis[i][j]=-1;
+						search(0);
+						if(count<temp2) {
+							x=i;y=j;ss=array[i][j];
+						}
+						array[i][j]='0';
+						vis[i][j]=0;
+					}else if(vis[i][j]==2) {
+						array[i][j]='1';
+						vis[i][j]=-1;
+						search(0);
+						if(count<temp2) {
+							x=i;y=j;ss=array[i][j];
+						}
+						array[i][j]='0';
+						vis[i][j]=0;
+					}
+				}	
+			}
+			if(x==-1||y==-1) {
+				System.out.println("wujie");
+			}else {
+				System.out.println(x+" "+y+" "+ss);
+			}
+			
 		}
- 
 	}
- 
-	public static boolean addrow(char[][] array, int i, int j) {
-		// left and right can't be '-'
-		if (j == 0) {
-			if (array[i][j + 1] != '-') {
-				System.out.println(i + "" + j + "" + '-');
-				return true;
+	public static void search(int num) {
+		init();
+		if(!isContinue()||num>=12) {
+			if(num%2==0&&num<count) {
+				count =num;
 			}
- 
-		} else if (j == 3) {
-			if (array[i][j - 1] != '-') {
-				System.out.println(i + "" + j + "" + '-');
-				return true;
-			}
-		} else {
-			if (array[i][j - 1] != '-' && array[i][j + 1] != '-') {
-				System.out.println(i + "" + j + "" + '-');
-				return true;
-			}
+			return;
+		}
+		for(int i=0;i<3;i++) {
+			for (int j = 0; j < 4; j++) {
+				if(vis[i][j]==0) {
+					array[i][j]='1';
+					vis[i][j]=-1;
+					search(num++);
+					array[i][j]='0';
+					vis[i][j]=0;
+					array[i][j]='-';
+					vis[i][j]=-1;
+					search(num++);
+					array[i][j]='0';
+					vis[i][j]=0;
+				}else if(vis[i][j]==1){
+					array[i][j]='-';
+					vis[i][j]=-1;
+					search(num++);
+					array[i][j]='0';
+					vis[i][j]=0;
+				}else if(vis[i][j]==2) {
+					array[i][j]='1';
+					vis[i][j]=-1;
+					search(num++);
+					array[i][j]='0';
+					vis[i][j]=0;
+				}
+			}	
+		}
+	}
+	public static boolean isContinue() {
+		for(int i=0;i<3;i++) {
+			for (int j = 0; j < 4; j++) {
+				if(vis[i][j]>0){
+					return true;
+				}
+			}	
 		}
 		return false;
 	}
- 
-	public static boolean addline(char[][] array, int i, int j) {
-		// up and bottom can't be '1'
-		if (i == 0) {
-			if (array[i + 1][j] != '1') {
-				System.out.println(i + "" + j + "" + 1);
-				return true;
-			}
- 
-		} else if (i == 2) {
-			if (array[i - 1][j] != '1') {
-				System.out.println(i + "" + j + "" + 1);
-				return true;
-			}
-		} else {
-			if (array[i - 1][j] != '1' && array[i + 1][j] != '1') {
-				System.out.println(i + "" + j + "" + '1');
-				return true;
+	//先将能放置的位置筛选出来
+	public static void init() {
+		for(int i=0;i<3;i++) {
+			for (int j = 0; j < 4; j++) {	
+				//等于零说明没有占用
+				if(vis[i][j]>=0) {
+					int a=j,b=j,c=i,d=i,cflag=0,lflag=0;
+					//向左扫描
+					a--;
+					while(a>=0) {
+						if(array[i][a]=='1') {
+							break;
+						}else if(array[i][a]=='-') {
+							cflag=1;
+							break;
+						}
+						a--;
+					}
+					//向右扫描
+					b++;
+					while(b<4) {
+						if(array[i][b]=='1') {
+							break;
+						}else if(array[i][b]=='-') {
+							cflag=1;
+							break;
+						}
+						b++;
+					}
+					//向上扫描
+					
+					c--;
+					while(c>=0) {
+						if(array[c][j]=='-') {
+							break;
+						}else if(array[c][j]=='1') {
+							lflag=1;
+							break;
+						}
+						c--;
+					}
+					
+					//向下扫描
+					d++;
+					while(d<3) {
+						if(array[d][j]=='-') {
+							break;
+						}else if(array[d][j]=='1') {
+							lflag=1;
+							break;
+						}
+						d++;
+					}
+					
+					if(cflag==1&&lflag==1) {
+						vis[i][j]=-1;
+					}else if (cflag==0&&lflag==1) {
+						vis[i][j]=1;
+					}else if(cflag==1&&lflag==0) {
+						vis[i][j]=2;
+					}
+					
+				}
 			}
 		}
-		return false;
 	}
 }
